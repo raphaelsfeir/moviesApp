0ac22b37f4900d427a6db22cb34cf454ae4f89d0
Raphaël Sfeir <rsfeir@hotmail.fr>
2019-08-24 17:48:38 +0200

    Saving movie to local storage

--- src/app/services/system.service.ts
+++ src/app/services/system.service.ts
@@ -12,7 +12,9 @@ export class SystemService {
   async getMem(key: string) {
     const mem = await this._storage.get(key);
     if (!mem) {
-      throw new Error('The key doesn\'t exist !');
+      throw new Error('key-not-exists');
+    } else {
+      return mem;
     }
   }
   setMem(key: string, value: any) {
@@ -22,18 +24,24 @@ export class SystemService {
     try {
       await this.getMem('saved');
     } catch (e) {
-      this.setMem('saved', []);
+      this.setMem('saved', {movies: [], tv: []});
     }
   }
-  async appendMem(key: string, add: any) {
-    const old = await this.getMem(key);
-    return this.setMem(key, [old, add]);
+  async appendMem(key: string, add: any, type?: 'movies' | 'tv') {
+    try {
+      const mem = await this.getMem(key);
+      mem[`${(!type) ? 'movies' : type}`].push(add);
+      return this.setMem(key, mem);
+    } catch (e) {
+      this.initMem();
+      const mem = await this.getMem(key);
+      mem[`${(!type) ? 'movies' : type}`].push(add);
+      return this.setMem(key, mem);
+    }
   }
-
-
   async save(id: number, type?: 'movies' | 'tv') {
     try {
-      await this.appendMem((!type) ? 'movies' : type, id);
+      await this.appendMem('saved', id, (!type) ? 'movies' : type);
       this._inter.toast('The movie has been saved');
     } catch (e) {
       console.error(e);
@@ -41,6 +49,6 @@ export class SystemService {
     }
   }
   async getStatus(id: number) {
-    return 'saved';
+
   }
 }
